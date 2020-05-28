fb04c1ad17e00ab1f43b7aa88454c38dad45dfe6
user3748 <gituser3748@protonmail.com>
2020-05-28 12:20:25 +0200

    added random number generator tests

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading;
+using System.Reflection.Metadata.Ecma335;
 
 namespace random_console_test_net_core
 {
@@ -6,8 +8,29 @@ namespace random_console_test_net_core
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            //init Random()
+            Random rnd = new Random();
+            int trys = 0;
+            int wait = 1000;//seconds to wait between each try.
+
+            //random lucky numbers
+            int x = rnd.Next(1, 101);//generate value from 1(inclusive) to 100(inclusive) lucky number 1
+            int y = rnd.Next(1, 101);//generate value from 1(inclusive) to 100(inclusive) lucky number 2
+            while (true)
+            {
+                trys++;
+                //simulating 2 percent chance
+                
+                int a = rnd.Next(1, 101);//random number generated
+                Console.WriteLine($"try: {trys} random number a: {a} lucky number x: {x} lucky number y: {y}");
+                if (a == x || a == y)
+                {
+                    Console.WriteLine($"random number a : {a} matched: x:{x} or y:{y} in {trys} trys");
+                    Console.WriteLine("Press any key to try again... Press Ctrl + C to exit this program.");
+                    Console.ReadKey();
+                }
+                Thread.Sleep(wait);//Sleep for x milliseconds
+            }
         }
     }
 }
--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 # ubuntu-latest build test
 [![Build Status](https://dev.azure.com/matzemail2434545/matzemail2434545/_apis/build/status/user3748.random-console-test-net-core?branchName=master)](https://dev.azure.com/matzemail2434545/matzemail2434545/_build/latest?definitionId=2&branchName=master)
+#info
+this program should run on windows and ubuntu if the net core sdk is installed
+
